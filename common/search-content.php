fef871e0673b24854339f78f12cb78d075a3fd50
Chen <zhangchen915@outlook.com>
2018-04-25 12:55:16 +0800

    style: punctuation

--- common/search-content.php
+++ common/search-content.php
@@ -3,9 +3,9 @@
         <article class="post type-post">
             <h2 class="post-title" itemprop="name headline"><a itemtype="url" href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
             <ul class="post-meta">
-                <li>时间: <time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date('Y-m-d'); ?></time></li>
-                <li>分类: <?php $this->category(','); ?></li>
-                <li><span>阅读: <?php $this->views(); ?>次</span></li>
+                <li>时间：<time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date('Y-m-d'); ?></time></li>
+                <li>分类：<?php $this->category(','); ?></li>
+                <li><span>阅读：<?php $this->views(); ?>次</span></li>
             </ul>
             <div class="post-content" itemprop="articleBody">
                 <?php $this->content('阅读全文&raquo;'); ?>
--- post.php
+++ post.php
@@ -6,8 +6,8 @@
         <h1 class="post-title" itemprop="name headline"><?php $this->title() ?></h1>
         <ul class="post-meta">
             <?php if(!is_mobile()) :?><li itemprop="author">作者: <a itemprop="name" href="<?php $this->author->permalink(); ?>" rel="author"><?php $this->author(); ?></a></li><?php endif; ?>
-            <li>时间: <time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date('Y-m-d'); ?></time></li>
-            <li>分类: <?php $this->category(','); ?></li>
+            <li>时间：<time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date('Y-m-d'); ?></time></li>
+            <li>分类：<?php $this->category(','); ?></li>
             <li><span>阅读：<?php get_post_view($this) ?>次</span></li>
 		    <li><span class="ds-thread-count" data-thread-key="<?php $this->cid(); ?>"></span></li>
         </ul>
@@ -18,7 +18,7 @@
 
         <div class="creative-commons <?php echo $this->options->CC ?>"></div>
        
-        <div itemprop="keywords" class="tags">标签: <?php $this->tags(', ', true, '无'); ?></div>
+        <div itemprop="keywords" class="tags">标签：<?php $this->tags(', ', true, '无'); ?></div>
         <?php $this->related(4)->to($relatedPosts); ?>
         <div class="related">
             <h3>相关文章推荐</h3>
@@ -30,8 +30,8 @@
         </div>
 
         <ul class="post-near">
-            <li>上一篇: <?php $this->thePrev('%s','没有了'); ?></li>
-            <li>下一篇: <?php $this->theNext('%s','没有了'); ?></li>
+            <li>上一篇：<?php $this->thePrev('%s','没有了'); ?></li>
+            <li>下一篇：<?php $this->theNext('%s','没有了'); ?></li>
         </ul>
     </article>
 
