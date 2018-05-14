fd9248d783fa0a704b3dfa0c4ddcc7fdba7b2efd
Chen <zhangchen915@outlook.com>
2018-05-14 22:55:55 +0800

    fix: drawer

--- src/drawer.js
+++ src/drawer.js
@@ -3,8 +3,13 @@ import {
 } from '@material/drawer';
 
 const toggle = document.querySelector('#toggle');
-const MDCdrawer = new MDCPersistentDrawer(document.querySelector('.mdc-drawer--persistent'));
+const drawerEl = document.querySelector('.mdc-drawer--persistent');
+const MDCdrawer = new MDCPersistentDrawer(drawerEl);
 toggle.addEventListener('click', e => {
     toggle.className = toggle.className ? '' : 'on';
     MDCdrawer.open = !MDCdrawer.open
+})
+
+drawerEl.addEventListener('MDCPersistentDrawer:close', () => {
+    if (toggle.className) toggle.className = '';
 })
