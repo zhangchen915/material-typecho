ad1e59a8bc303354754324a7eb0f752cf45d9ec5
Chen <zhangchen915@outlook.com>
2018-05-20 11:49:12 +0800

    fix: Skeleton Screen

--- common/sidebar.php
+++ common/sidebar.php
@@ -1,4 +1,5 @@
 <aside class="mdc-layout-grid__cell mdc-layout-grid__cell--span-3 mdc-layout-grid__cell--span-4-phone" role="complementary">
+<?php if(!is_mobile()) :?>
     <?php if (!empty($this->options->sidebarBlock) && in_array('ShowCategory', $this->options->sidebarBlock)): ?>
         <section class="widget">
             <h3>文章分类</h3>
@@ -16,39 +17,38 @@
             </ul>
         </section>
     <?php endif; ?>
+<?php endif; ?>
 
-    <?php if(!is_mobile()) :?>
-        <?php if (!empty($this->options->sidebarBlock) && in_array('ShowRecentPosts', $this->options->sidebarBlock)): ?>
-        <section class="widget">
-            <h3>最新文章</h3>
-            <ul class="mdc-list mdc-list--dense">
-                <?php $this->widget('Widget_Contents_Post_Recent','pageSize=5')
-                ->parse('<li class="mdc-list-item"><a href="{permalink}">{title}</a></li>'); ?>
-            </ul>
-        </section>
-        <?php endif; ?>
+<?php if (!empty($this->options->sidebarBlock) && in_array('ShowRecentPosts', $this->options->sidebarBlock)): ?>
+<section class="widget">
+    <h3>最新文章</h3>
+    <ul class="mdc-list mdc-list--dense">
+        <?php $this->widget('Widget_Contents_Post_Recent','pageSize=5')
+        ->parse('<li class="mdc-list-item"><a href="{permalink}">{title}</a></li>'); ?>
+    </ul>
+</section>
+<?php endif; ?>
 
-        <?php if (!empty($this->options->sidebarBlock) && in_array('ShowArchive', $this->options->sidebarBlock)): ?>
-            <section class="widget">
-                <h3>归档</h3>
-                <div class="mdc-select">
-                    <select class="mdc-select__native-control">
-                        <option value="">请选择归档</option>
-                        <?php $this->widget('Widget_Contents_Post_Date', 'type=month&format=F Y')
-                        ->parse('<option value="{permalink}">{date}</option>'); ?>
-                    </select>
-                    <div class="mdc-line-ripple"></div>
-                </div>
-            </section>
-        <?php endif; ?>
+<?php if (!empty($this->options->sidebarBlock) && in_array('ShowArchive', $this->options->sidebarBlock)): ?>
+    <section class="widget">
+        <h3>归档</h3>
+        <div class="mdc-select">
+            <select class="mdc-select__native-control">
+                <option value="">请选择归档</option>
+                <?php $this->widget('Widget_Contents_Post_Date', 'type=month&format=F Y')
+                ->parse('<option value="{permalink}">{date}</option>'); ?>
+            </select>
+            <div class="mdc-line-ripple"></div>
+        </div>
+    </section>
+<?php endif; ?>
 
-        <?php if (!empty($this->options->sidebarBlock) && in_array('ShowTags', $this->options->sidebarBlock)): ?>
-            <section class="widget widget-tag">
-                <h3>热门标签</h3>
-                <div class="tag-cloud">
-                    <?php $this->widget('Widget_Metas_Tag_Cloud', 'ignoreZeroCount=true&limit=30') ->parse('<a href="{permalink}" class="tag-link-{count}">{name} ({count})</a>'); ?>
-                </div>
-            </section>
-        <?php endif; ?>
-    <?php endif; ?>
+<?php if (!empty($this->options->sidebarBlock) && in_array('ShowTags', $this->options->sidebarBlock)): ?>
+    <section class="widget widget-tag">
+        <h3>热门标签</h3>
+        <div class="tag-cloud">
+            <?php $this->widget('Widget_Metas_Tag_Cloud', 'ignoreZeroCount=true&limit=30') ->parse('<a href="{permalink}" class="tag-link-{count}">{name} ({count})</a>'); ?>
+        </div>
+    </section>
+<?php endif; ?>
 </aside>
